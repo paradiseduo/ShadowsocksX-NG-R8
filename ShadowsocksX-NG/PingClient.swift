eb56a15fbd6ace243e260b22c7dbd0dea33fb255
ParadiseDuo <ParadiseDuo>
2020-03-21 13:22:25 +0800

    优化ping测试稳定性及性能，删除无用代码

--- ShadowsocksX-NG/PingClient.swift
+++ ShadowsocksX-NG/PingClient.swift
@@ -20,61 +20,14 @@ fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
   }
 }
 
-
 public typealias SimplePingClientCallback = (String?)->()
 
-
-
+var neverSpeedTestBefore:Bool = true
 
 class PingServers:NSObject{
     static let instance = PingServers()
     
     let SerMgr = ServerProfileManager.instance
-    var fastest:String?
-    var fastest_id : Int=0
-    
-    //    func ping(_ i:Int=0){
-    //        if i == 0{
-    //            fastest_id = 0
-    //            fastest = nil
-    //        }
-    //
-    //        if i >= SerMgr.profiles.count{
-    //            DispatchQueue.main.async {
-    //                // do the UI update HERE
-    //                let notice = NSUserNotification()
-    //                notice.title = "Ping测试完成！"
-    //                notice.subtitle = "最快的是\(self.SerMgr.profiles[self.fastest_id].remark) \(self.SerMgr.profiles[self.fastest_id].serverHost) \(self.SerMgr.profiles[self.fastest_id].latency!)ms"
-    //                NSUserNotificationCenter.default.deliver(notice)
-    //            }
-    //            return
-    //        }
-    //        let host = self.SerMgr.profiles[i].serverHost
-    //        SimplePingClient.pingHostname(host) { latency in
-    //            DispatchQueue.global().async {
-    //            print("[Ping Result]-\(host) latency is \(latency ?? "fail")")
-    //            self.SerMgr.profiles[i].latency = latency ?? "fail"
-    //
-    //            if latency != nil {
-    //                if self.fastest == nil{
-    //                    self.fastest = latency
-    //                    self.fastest_id = i
-    //                }else{
-    //                    if Int(latency!) < Int(self.fastest!) {
-    //                        self.fastest = latency
-    //                        self.fastest_id = i
-    //                    }
-    //                }
-    //                DispatchQueue.main.async {
-    //                    // do the UI update HERE
-    //                    (NSApplication.shared().delegate as! AppDelegate).updateServersMenu()
-    //                    (NSApplication.shared().delegate as! AppDelegate).updateRunningModeMenu()
-    //                }
-    //            }
-    //            }
-    //            self.ping(i+1)
-    //        }
-    //    }
     
     func runCommand(cmd : String, args : String...) -> (output: [String], error: [String], exitCode: Int32) {
         
@@ -122,123 +75,58 @@ class PingServers:NSObject{
         return latency
     }
     
-    func pingSingleHost(host:String,completionHandler:@escaping (Double?) -> Void){
-        DispatchQueue.global(qos: .userInteractive).async {
-            if let outputString = self.runCommand(cmd: "/sbin/ping", args: "-c","5","-t","2",host).output.last{
-                completionHandler(self.getlatencyFromString(result: outputString))
-            }
+    func ping(){
+        if SerMgr.profiles.count <= 0 {
+            return
         }
-    }
-    
-    
-
-    func ping(_ i:Int=0){
         
         neverSpeedTestBefore = false
         
-        var result:[(Int,Double)] = []
-        
-        NotificationCenter.default.addObserver(forName: NSNotification.Name("PingTestFinish"), object: nil, queue: OperationQueue.main) { (noti) in
-            DispatchQueue.main.async {
-                
-                for k in 0..<self.SerMgr.profiles.count {
-                    if let late = self.SerMgr.profiles[k].latency{
-                        if let latency = Double(late){
-                            result.append((k,latency))
-                        }
+        let group = DispatchGroup()
+        let queue = DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive)
+        for i in 0..<SerMgr.profiles.count {
+            group.enter()
+            queue.async {
+                if let outputString = self.runCommand(cmd: "/sbin/ping", args: "-c","5","-t","2",self.SerMgr.profiles[i].serverHost).output.last {
+                    if let latency = self.getlatencyFromString(result: outputString) {
+                        self.SerMgr.profiles[i].latency = String(latency)
                     }
                 }
-
-                // do the UI update HERE
-                if let min = result.min(by: {$0.1 < $1.1}){
-                    self.fastest = String(describing: min.1)
-                    self.fastest_id  = min.0
-                    let notice = NSUserNotification()
-                    notice.title = "Ping测试完成！最快\(self.SerMgr.profiles[self.fastest_id].latency!)ms"
-                    notice.subtitle = "最快的是\(self.SerMgr.profiles[self.fastest_id].serverHost) \(self.SerMgr.profiles[self.fastest_id].remark)"
-                    
-                    NSUserNotificationCenter.default.deliver(notice)
-                    
-                    UserDefaults.standard.setValue("\(self.SerMgr.profiles[self.fastest_id].latency!)", forKey: "FastestNode")
-                    UserDefaults.standard.synchronize()
-                    
-                    DispatchQueue.main.async {
-                        (NSApplication.shared.delegate as! AppDelegate).updateServersMenu()
-                        (NSApplication.shared.delegate as! AppDelegate).updateRunningModeMenu()
-                    }
-                }
-                
+                group.leave()
             }
         }
+        group.notify(queue: DispatchQueue.main) {
+            self.sortSpeed()
+        }
+    }
+    
+    func sortSpeed() {
+        var fastID = 0
+        var fastTime = Double.infinity
         
-        var testResult = 0
-        var haspostNotification = false
         for k in 0..<SerMgr.profiles.count {
-            let host = self.SerMgr.profiles[k].serverHost
-            pingSingleHost(host: host, completionHandler: { [weak self] in
-                guard let w = self else {return}
-                if let latency = $0{
-                    testResult += 1
-                    w.SerMgr.profiles[k].latency = String(latency)
-                    if testResult == w.SerMgr.profiles.count-1 {
-                        haspostNotification = true
-                        DispatchQueue.main.async {
-                            NotificationCenter.default.post(name: NSNotification.Name("PingTestFinish"), object: nil)
-                        }
-                    }
+            if let late = SerMgr.profiles[k].latency{
+                if let latency = Double(late), latency < fastTime {
+                    fastTime = latency
+                    fastID = k
                 }
-            })
+            }
         }
         
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+3) {
-            if !haspostNotification {
-                NotificationCenter.default.post(name: NSNotification.Name("PingTestFinish"), object: nil)
+        if fastTime != Double.infinity {
+            let notice = NSUserNotification()
+            notice.title = "Ping测试完成！最快\(SerMgr.profiles[fastID].latency!)ms"
+            notice.subtitle = "最快的是\(SerMgr.profiles[fastID].serverHost) \(SerMgr.profiles[fastID].remark)"
+            
+            NSUserNotificationCenter.default.deliver(notice)
+            
+            UserDefaults.standard.setValue("\(SerMgr.profiles[fastID].latency!)", forKey: "FastestNode")
+            UserDefaults.standard.synchronize()
+            
+            DispatchQueue.main.async {
+                (NSApplication.shared.delegate as! AppDelegate).updateServersMenu()
+                (NSApplication.shared.delegate as! AppDelegate).updateRunningModeMenu()
             }
         }
     }
 }
-
-
-typealias Task = (_ cancel : Bool) -> Void
-
-@discardableResult func delay(_ time: TimeInterval, task: @escaping ()->()) ->  Task? {
-    
-    func dispatch_later(block: @escaping ()->()) {
-        let t = DispatchTime.now() + time
-        DispatchQueue.main.asyncAfter(deadline: t, execute: block)
-    }
-    
-    
-    
-    var closure: (()->Void)? = task
-    var result: Task?
-    
-    let delayedClosure: Task = {
-        cancel in
-        if let internalClosure = closure {
-            if (cancel == false) {
-                DispatchQueue.main.async(execute: internalClosure)
-            }
-        }
-        closure = nil
-        result = nil
-    }
-    
-    result = delayedClosure
-    
-    dispatch_later {
-        if let delayedClosure = result {
-            delayedClosure(false)
-        }
-    }
-    
-    return result;
-    
-}
-
-
-func cancel(_ task: Task?) {
-    task?(true)
-}
-
-var neverSpeedTestBefore:Bool = true
