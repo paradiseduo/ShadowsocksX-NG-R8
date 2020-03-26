f0672a63ce5e5303a8b9c1c0a07e9781c8167fa0
ParadiseDuo <ParadiseDuo>
2020-03-26 15:46:53 +0800

    add terminate

--- LaunchAtLoginHelper/LaunchAtLoginHelper/AppDelegate.m
+++ LaunchAtLoginHelper/LaunchAtLoginHelper/AppDelegate.m
@@ -44,6 +44,8 @@
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(30 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [NSApp terminate:nil];
         });
+    } else {
+        [NSApp terminate:nil];
     }
 }
 
